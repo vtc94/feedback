d3086988ff15cff716166dc0eb7bf8093f9e30ab
vtc94 <0h.y3ar@gmail.com>
2020-10-23 12:07:41 +0930

    Update main.js

--- main.js
+++ main.js
@@ -20,13 +20,13 @@ function saveFeedback(){
 		db.ref('feedback/' + date.getTime()).set(newBlog);
 		
 		document.getElementById('notice').innerHTML = "Feedback has been sent, return to PTELT in 2s...";
-		document.getElementById('notice').style.display = 'initial';
+		document.getElementById('notice').style.display = 'block';
 		setTimeout(() => {
-			window.location.back();
+			window.location.back;
 		}, 2000);
 	} else {
 		document.getElementById('notice').innerHTML = "Cannot send feedback, something is missing...";
-		document.getElementById('notice').style.display = 'initial';
+		document.getElementById('notice').style.display = 'block';
 		setTimeout(() => {
 			document.getElementById('notice').style.display = 'none';
 		}, 2000);
